974ad46fc65ec8ea47acc93135b48bcd373d431f
Dxnrojas <drojasaristi19@gmail.com>
2024-09-25 19:59:39 -0500

    chore(build): update componet cardpost: css/responsive

--- public/src/data/data.ts
+++ public/src/data/data.ts
@@ -7,7 +7,7 @@ export const data = [
         profileDesc: 'Amante de los paseos largos y los huesos.',
         likes: 200,
         comments: 40,
-        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost1.png?alt=media&token=dummytoken1',
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/imagen%20perrito%201.png?alt=media&token=51a95099-3d1b-4148-8a9c-53ee57926fdb',
         postDesc: 'Disfrutando de un día soleado en el parque con mi pelota favorita!',
         profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
     },
@@ -19,7 +19,7 @@ export const data = [
         profileDesc: 'Dueña del sofá y del corazón de todos.',
         likes: 150,
         comments: 30,
-        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost2.png?alt=media&token=dummytoken2',
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/imagen%20perrito%202.png?alt=media&token=d5f9d9af-3e01-4f34-b397-615a319a6d58',
         postDesc: 'Aquí, posando en mi lugar favorito para dormir. ¡Nada como una buena siesta!',
         profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
     },
@@ -31,7 +31,7 @@ export const data = [
         profileDesc: 'Explorando el mundo, una aventura a la vez.',
         likes: 220,
         comments: 50,
-        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost3.png?alt=media&token=dummytoken3',
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/imagen%20perrito%203.png?alt=media&token=9b75c8f1-46cd-4958-92f0-263b3132c830',
         postDesc: 'Descubriendo nuevos senderos y olfateando todo lo que encuentro. ¡Amo la naturaleza!',
         profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
     },
@@ -43,7 +43,7 @@ export const data = [
         profileDesc: 'Amo mis siestas y el sol.',
         likes: 188,
         comments: 35,
-        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost4.png?alt=media&token=dummytoken4',
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/imagen%20perrito%204.png?alt=media&token=b234d844-a6b8-4bb6-a29a-6af331c3c307',
         postDesc: '¡El lugar perfecto para una siesta bajo el sol! No me molesten.',
         profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
     }
