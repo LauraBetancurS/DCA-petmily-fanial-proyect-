fd05bc5578824628cffee1e5003b4da37d48b9b7
Dxnrojas <drojasaristi19@gmail.com>
2024-11-05 15:30:33 -0500

    chore: infoPost types to modified de attributes

--- src/screens/createpost/post.ts
+++ src/screens/createpost/post.ts
@@ -1,5 +1,11 @@
 import {addPost, getPost} from '../../utils/firebase'
 
+const infoPosts= {
+    uid: '',
+    image: '',
+    description: ''
+}
+
 class CreatePost extends HTMLElement{
     constructor(){
         super();
