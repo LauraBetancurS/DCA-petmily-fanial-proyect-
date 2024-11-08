5272191caecb95cad67656c3d9ab0a6b7cd50293
LauraBetancurS <lau1betancur1@gmail.com>
2024-11-08 07:56:48 -0500

    chore:add navbar componnet import

--- src/screens/createpost/post.ts
+++ src/screens/createpost/post.ts
@@ -4,6 +4,8 @@ import { Screens } from "../../types/store";
 import { addPost, getFileUrls, getUser, uploadFile } from "../../utils/firebase";
 import styles from "./posts.css";
 import { data } from "../../data/data";
+import "../../components/navbar/navbar";
+
 
 
 const infoPosts: {
