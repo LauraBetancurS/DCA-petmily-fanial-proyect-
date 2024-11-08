7a62b3f8fcdb92ec7d54fcbf4ad4ec3f20d84397
LauraBetancurS <lau1betancur1@gmail.com>
2024-11-08 08:31:13 -0500

    ui:image-upload-container

--- src/screens/createpost/post.ts
+++ src/screens/createpost/post.ts
@@ -2,7 +2,7 @@ import { appState, dispatch, addObserver } from "../../store";
 import { navigate } from "../../store/actions";
 import { Screens } from "../../types/store";
 import { addPost, getFileUrls, getUser, uploadFile } from "../../utils/firebase";
-import styles from "./createpost.css";
+import styles from "./posts.css";
 import "../../components/navbar/navbar";
 import "../../components/banner/banner";
 
