e9b5f8fde93eaea8b5c09e74eb2ed0ab11e789a4
Dxnrojas <drojasaristi19@gmail.com>
2024-11-27 17:12:55 -0500

    chore: delete files

--- src/components/indexPadre.ts
+++
@@ -1,5 +0,0 @@
-export { default as NavBar } from './navbar/navbar';
-export { default as UserMenu } from './usermenu/usercard'; // Añadido este componente
-export {default as CardPost} from './cardspost/cardpost';
-export {default as PublicityCard} from './publicitycard/publicitycard';
-export {default as Banner} from './banner/banner';
