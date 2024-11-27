086ae935357b4469000585bd70db603eab0e8628
LauraBetancurS <lau1betancur1@gmail.com>
2024-11-27 17:55:51 -0500

    update: card post component

--- src/components/cardspost/cardpost.ts
+++ src/components/cardspost/cardpost.ts
@@ -1,3 +1,11 @@
+//cardpost.ts
+
+
+import { dispatch } from "../../store";
+import { navigate } from "../../store/actions";
+import { Screens } from "../../types/store";
+
+
 export enum Attribute {
     "username" = "username",
     "name" = "name",
@@ -5,6 +13,8 @@ export enum Attribute {
     "postdesc" = "postdesc",
     "imgpost" = "imgpost",
 }
+
+
 
 
 class CardPost extends HTMLElement {
@@ -15,14 +25,18 @@ class CardPost extends HTMLElement {
     imgpost?: string;
 
 
+
+
     constructor() {
         super();
         this.attachShadow({ mode: 'open' })
     }
 
+
     static get observedAttributes() {
         return Object.keys(Attribute);
     }
+
 
     attributeChangedCallback(propName: Attribute, oldValue: string | undefined, newValue: string | undefined) {
         if (newValue !== oldValue) {
@@ -30,11 +44,21 @@ class CardPost extends HTMLElement {
             this.render();
         }
     }
-    
+   
+
 
     connectedCallback() {
         this.render()
+        const usernameElement = this.shadowRoot?.querySelector('.username');
+        if (usernameElement) {
+            usernameElement.addEventListener('click',()=> {
+                console.log(`Username clicked: ${this.username}`);
+                dispatch(navigate(Screens.PROFILE, {username : this.username}))
+               
+            })
+        }
     }
+
 
     render() {
         if (this.shadowRoot) {
@@ -42,17 +66,19 @@ class CardPost extends HTMLElement {
             <link rel="stylesheet" href="../src/components/cardspost/cardpost.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
 
+
             <article class='cardpost-container'>
                 <section class='post-container'>
                     <div class='profile-icon'
                         <img src="${this.profileimg || "no image"}">
                     </div>
                     <div class='info-post'>
-                        <h4>${this.name || "no name"}</h4>
+                        <h4 class="username">${this.name || "no name"}</h4>
                         <h5>@${this.username || "no user"}</h5>
                         <p>${this.postdesc || "no description"}</p>
                     </div>
                 </section>
+
 
                 <div class='image-post'>
                     <img src="${this.imgpost || "no post"}" />
@@ -61,13 +87,11 @@ class CardPost extends HTMLElement {
                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-heart"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-message-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 20l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c3.255 2.777 3.695 7.266 1.029 10.501c-2.666 3.235 -7.615 4.215 -11.574 2.293l-4.7 1" /></svg>
                 </div>
-            
-            </article>
-            
            
-            
+            </article>
             `
         }
+
 
     }
 }
