af3e4ee7d64318a68aae10d22adfac103f03ce85
LauraBetancurS <lau1betancur1@gmail.com>
2024-11-29 13:47:03 -0500

    chore: clean up code and anotations remove console logss on profile.ts

--- src/screens/profile/profile.ts
+++ src/screens/profile/profile.ts
@@ -1,5 +1,10 @@
-import { addObserver, appState } from "../../store";
-import { getPost, getUserByUsername, getDocumentIdByUsername, getFileUrlProfileImg } from "../../utils/firebase";
+import { addObserver } from "../../store";
+import {
+  getPost,
+  getUserByUsername,
+  getDocumentIdByUsername,
+  getFileUrlProfileImg,
+} from "../../utils/firebase";
 import "../../components/navbar/navbar";
 import "../../components/banner/banner";
 import "../../components/publicitycard/publicitycard";
@@ -21,21 +26,20 @@ class Profile extends HTMLElement {
   async connectedCallback() {
     this.username = this.getAttribute("username") || "";
     if (this.username) {
-      this.userId = await getDocumentIdByUsername(this.username) || "";
+      this.userId = (await getDocumentIdByUsername(this.username)) || "";
       this.render();
-    } else {
-      console.error("Username not defined for app-profile");
     }
   }
 
   async render() {
     if (!this.shadowRoot) return;
 
+    const isMobileView = window.innerWidth <= 768;
+
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="../src/screens/profile/profile.css">
     `;
 
-    // Navbar
     const navBar = this.ownerDocument.createElement("nav-bar");
     navBar.setAttribute("icon", "http://imgfz.com/i/DjpNIAU.png");
     navBar.setAttribute("input", "Search PetNet");
@@ -49,7 +53,6 @@ class Profile extends HTMLElement {
       "https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/icono%20lupa.png?alt=media&token=16d3b4ec-5267-407c-8b63-a46f3bdba029"
     );
 
-    // Banner
     const banner = this.ownerDocument.createElement("app-banner");
     banner.className = "banner";
     banner.setAttribute(
@@ -57,12 +60,12 @@ class Profile extends HTMLElement {
       "https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/banner%20componenet.png?alt=media&token=19fb1727-6c11-4281-8723-c0100079d0be"
     );
 
-    // Main container
     const mainContainer = this.ownerDocument.createElement("section");
-    mainContainer.className = "main-container";
+    mainContainer.className = `main-container ${
+      isMobileView ? "responsive-background" : ""
+    }`;
 
-    // Left Sidebar
-    const leftSidebar = this.ownerDocument.createElement("div");
+    const leftSidebar = this.ownerDocument.createElement("aside");
     leftSidebar.className = "left-sidebar";
 
     const userCard = this.ownerDocument.createElement("user-banner");
@@ -70,8 +73,9 @@ class Profile extends HTMLElement {
     if (this.userId) {
       try {
         const userData = await getUserByUsername(this.username);
-        let profilePicUrl = await getFileUrlProfileImg(this.userId).catch(() =>
-          "https://i.pinimg.com/474x/31/ec/2c/31ec2ce212492e600b8de27f38846ed7.jpg"
+        const profilePicUrl = await getFileUrlProfileImg(this.userId).catch(
+          () =>
+            "https://i.pinimg.com/474x/31/ec/2c/31ec2ce212492e600b8de27f38846ed7.jpg"
         );
 
         userCard.setAttribute("profilepic", profilePicUrl);
@@ -82,22 +86,22 @@ class Profile extends HTMLElement {
 
         leftSidebar.appendChild(userCard);
       } catch (error) {
-        console.error(error);
+        console.error("Error loading user data:", error);
       }
     }
 
-    // UserCard for responsive (top-user-menu)
-    const topUserMenu = this.ownerDocument.createElement("div");
+    const topUserMenu = this.ownerDocument.createElement("section");
     topUserMenu.className = "top-user-menu";
-    topUserMenu.appendChild(userCard.cloneNode(true)); // Clone userCard for the responsive menu
+    topUserMenu.appendChild(userCard.cloneNode(true));
 
-    // Content Container
-    const contentContainer = this.ownerDocument.createElement("div");
+    const contentContainer = this.ownerDocument.createElement("main");
     contentContainer.className = "content-container";
 
     const userPosts = await getPost(this.username);
     userPosts?.forEach((post: dashboardPost) => {
-      const cardPost = this.ownerDocument.createElement("card-post") as CardPost;
+      const cardPost = this.ownerDocument.createElement(
+        "card-post"
+      ) as CardPost;
       cardPost.setAttribute(Attribute.name, post.name);
       cardPost.setAttribute(Attribute.username, post.username);
       cardPost.setAttribute(Attribute.profileimg, post.profileImg);
@@ -106,8 +110,7 @@ class Profile extends HTMLElement {
       contentContainer.appendChild(cardPost);
     });
 
-    // Right Sidebar
-    const rightSidebar = this.ownerDocument.createElement("div");
+    const rightSidebar = this.ownerDocument.createElement("aside");
     rightSidebar.className = "right-sidebar";
 
     const publicityCard = this.ownerDocument.createElement("publicity-card");
@@ -121,15 +124,14 @@ class Profile extends HTMLElement {
     );
     publicityCard.setAttribute(
       "img",
-      "https://firebasestorage.googleapis.com/v0/b/petmily-7b24c.appspot.com/o/Assets%20Dash%2Ffoto1%201.png?alt=media&token=2db66df2-6b62-4c65-bcdf-c82b4acad636"
+      "https://firebasestorage.googleapis.com/v0/b/dca-petmily.appspot.com/o/Assets%20Dash%2Ffoto1%201.png?alt=media&token=2db66df2-6b62-4c65-bcdf-c82b4acad636"
     );
 
     rightSidebar.appendChild(publicityCard);
 
-    // Append sections
     this.shadowRoot.appendChild(navBar);
-    this.shadowRoot.appendChild(banner); // Conserva el banner
-    this.shadowRoot.appendChild(topUserMenu); // UserCard for responsive
+    this.shadowRoot.appendChild(banner);
+    this.shadowRoot.appendChild(topUserMenu);
     this.shadowRoot.appendChild(mainContainer);
     mainContainer.appendChild(leftSidebar);
     mainContainer.appendChild(contentContainer);
